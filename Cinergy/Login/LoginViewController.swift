69230be3d90efdda364d812aa8e0252c331b91bd
Sachin Dinesh <sachindin4@gmail.com>
2024-01-21 00:15:05 -0600

    code cleanup in login view controller

--- Cinergy/Login/LoginViewController.swift
+++ Cinergy/Login/LoginViewController.swift
@@ -12,46 +12,46 @@ class LoginViewController: UIViewController {
     var viewModel: LoginViewModel!
     var userModel: LoginModel.UserModel?
     @IBAction func continueButtonClicked(_ sender: Any) {
-        viewModel.performLogin(completion: { result in
-            DispatchQueue.main.async {
-                switch result{
-                case .success(let loginModel):
-                    self.userModel = loginModel.user
-                    if let homeVC = self.storyboard?.instantiateViewController(withIdentifier: "HomeViewController") as? HomeViewController {
-                        homeVC.moviesListViewModel.userModel = self.userModel
-                        self.navigationController?.pushViewController(homeVC, animated: true)
-                    }
-                case .failure(_): break
-                    //API Fail Show error
-                }
-            }
+        callGuestTokenAPI(completion: {
+            self.callLoginAPI()
         })
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         viewModel = LoginViewModel()
-        viewModel.getGuestToken(completion: {result in
-            switch result{
-            case .success(let guestTokenModel): break
-                //API Success WIP
-            case .failure(let error): break
-                //API Fail WIP
+    }
+    
+    func callGuestTokenAPI(completion: @escaping() -> Void) {
+        viewModel.getGuestToken(completion: {_ in
+            completion()
+        })
+    }
+    
+    func callLoginAPI() {
+        self.viewModel.performLogin(completion: { result in
+            DispatchQueue.main.async {
+                switch result{
+                case .success(let loginModel):
+                    self.navigateToHomeScreen(loginModel: loginModel)
+                case .failure(_):
+                    self.showFailureAlertView()
+                }
             }
         })
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func navigateToHomeScreen(loginModel: LoginModel) {
+        self.userModel = loginModel.user
+        if let homeVC = self.storyboard?.instantiateViewController(withIdentifier: "HomeViewController") as? HomeViewController {
+            homeVC.moviesListViewModel.userModel = self.userModel
+            self.navigationController?.pushViewController(homeVC, animated: true)
+        }
     }
-    */
-
+    
+    func showFailureAlertView() {
+        let alert = UIAlertController(title: "Login Failed", message: "Something went wrong. Please try agin later.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        self.present(alert, animated: true)
+    }
 }
